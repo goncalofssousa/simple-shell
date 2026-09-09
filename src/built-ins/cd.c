29f630aa0013fefb4646c58ba558dee99c83145f
Goncalo Sousa <goncalofssousa@gmail.com>
2026-09-09 14:41:16 +0100

    cd sem flags implementado

--- src/built-ins/cd.c
+++ src/built-ins/cd.c
@@ -1,12 +1,21 @@
 #include "built-ins/cd.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int cd(char **args){
-    if(args[1] == NULL){
-        printf("Cd sem args\n"); 
+int cd(char **args) {
+    char *home = getenv("HOME");
+    if (args[1] == NULL || (args[1][0] == '~' && args[1][1] == '\0')) {
+        if (chdir(home) < 0) {
+            printf("cd: No such file or directory\n");
+            return 1;
+        }
     } else {
-        printf("Cd com args\n"); 
+        if (chdir(args[1]) < 0) {
+            printf("cd: No such file or directory\n");
+            return 1;
+        }
     }
-    return 1; 
+
+    return 0;
 }
